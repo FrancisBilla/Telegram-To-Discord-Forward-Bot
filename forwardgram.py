04bcb20e855818bd7d7e384683c556cda4ad1db5
FrancisBilla <francis.billa.fb@gmail.com>
2022-02-06 12:41:59 +0000

    customize Bot

--- forwardgram.py
+++ forwardgram.py
@@ -6,7 +6,7 @@ import logging
 import discord
 import subprocess
 
-''' 
+'''
 ------------------------------------------------------------------------
                 LOGGING - Initite logging for the Bot
 ------------------------------------------------------------------------
@@ -15,15 +15,15 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(level
 logging.getLogger('telethon').setLevel(level=logging.WARNING)
 logger = logging.getLogger(__name__)
 
-''' 
+'''
 ------------------------------------------------------------------------
     BOT FUNCTION - Everything that happens, happens for a reason
 ------------------------------------------------------------------------
 '''
 def start(config):
     # Telegram Client Init
-    client = TelegramClient(config["session_name"], 
-                            config["api_id"], 
+    client = TelegramClient(config["session_name"],
+                            config["api_id"],
                             config["api_hash"])
     # Telegram Client Start
     client.start()
@@ -39,7 +39,7 @@ def start(config):
         if d.name in config["output_channel_names"] or d.entity.id in config["output_channel_ids"]:
             output_channel_entities.append(InputChannel(d.entity.id, d.entity.access_hash))
 
-    # Exit, dont wait for fire.        
+    # Exit, dont wait for fire.
     if not output_channel_entities:
         logger.error(f"Could not find any output channels in the user's dialogs")
         sys.exit(1)
@@ -47,10 +47,8 @@ def start(config):
     if not input_channels_entities:
         logger.error(f"Could not find any input channels in the user's dialogs")
         sys.exit(1)
-    
-    # Use logging and print messages on your console.     
+    # Use logging and print messages on your console.
     logging.info(f"Listening on {len(input_channels_entities)} channels. Forwarding messages to {len(output_channel_entities)} channels.")
-    
 
     # TELEGRAM NEW MESSAGE - When new message triggers, come here
 
@@ -68,7 +66,7 @@ def start(config):
             try:
                 parsed_response = (event.message.message + '\n' + event.message.entities[0].url )
                 parsed_response = ''.join(parsed_response)
-            # Or else we only send Message    
+            # Or else we only send Message
             except:
                 parsed_response = event.message.message
 
@@ -76,11 +74,11 @@ def start(config):
             # When message triggers you start discord messanger script in new thread and sends parsed input as sys.argv[1]
             subprocess.call(["python", "discord_messager.py", str(parsed_response)])
             # this will forward your message to channel_recieve in Telegram
-            await client.forward_messages(output_channel, event.message)  
+            await client.forward_messages(output_channel, event.message)
 
     client.run_until_disconnected()
 
-''' 
+'''
 ------------------------------------------------------------------------
           MAIN FUNCTION - Can't dream without a brain ...
 ------------------------------------------------------------------------
